2afb12f8d13c42c766670f174312b2e28a8f8ca5
bhanushishodia <mobiweb.developer2@gmail.com>
2025-12-11 17:18:11 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,23 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request: any) {
-  const user = request.cookies.get("user")?.value;
+  // const user = request.cookies.get("user")?.value;
+  const localAuth = request.cookies.get("localAuth")?.value;
+
   const role = request.cookies.get("role")?.value;
 
   const protectedRoutes = ["/dashboard"];
 
   if (protectedRoutes.some((route) => request.nextUrl.pathname.startsWith(route))) {
-    if (!user) {
+    // if (!user) {
+    //   const loginUrl = new URL("/", request.url);
+    //   return NextResponse.redirect(loginUrl);
+    // }
+    if (!localAuth) {
       const loginUrl = new URL("/", request.url);
       return NextResponse.redirect(loginUrl);
     }
+
   }
 
   return NextResponse.next();
